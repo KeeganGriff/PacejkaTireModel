2d68a8acd2e82e888fcc345cf7626adcaec635fd
19patris <sam.eva.patrick@gmail.com>
2024-03-15 12:38:24 -0700

    practicing pandas lol

--- calculation_rough.py
+++ calculation_rough.py
@@ -1,4 +1,4 @@
-# D = peak lateral force (abs val)
+# D = peak lateral force (abs val) (newtons)
 # Ya = value approached asymptotically as x increases (right of D)
 # C = 1 +- (1 - (2/pi)(arcsin(Ya/D)))
 # then find point on curve closest to (0,0), and its slope. its slope = BCD
@@ -7,3 +7,8 @@
 # E = (BXm - tan(pi/2C))/(BXm - arctan(BXm)), if C > 1
 
 # now we can calculate y = Dsin[C*arctan(Bx - E{Bx - arctan})], where x is slip angle
+
+import pandas as pd
+
+df = pd.read_csv("B2356raw5.csv", sep='\t', skiprows=[0,2], usecols=['SA', 'FY'])
+print(df)
