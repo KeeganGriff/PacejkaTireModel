97a6c103636cdf11ffb05f6fa9abb559552b68d7
19patris <sam.eva.patrick@gmail.com>
2024-03-15 11:20:39 -0700

    adding calculation method

---
+++ calculation_rough.py
@@ -0,0 +1,9 @@
+# D = peak lateral force (abs val)
+# Ya = value approached asymptotically as x increases (right of D)
+# C = 1 +- (1 - (2/pi)(arcsin(Ya/D)))
+# then find point on curve closest to (0,0), and its slope. its slope = BCD
+# then B = BCD/CD
+# Xm is x value of peak D
+# E = (BXm - tan(pi/2C))/(BXm - arctan(BXm)), if C > 1
+
+# now we can calculate y = Dsin[C*arctan(Bx - E{Bx - arctan})], where x is slip angle
